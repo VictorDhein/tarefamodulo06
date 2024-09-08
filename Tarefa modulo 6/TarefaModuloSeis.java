057f459ca51ba35380f00b00d1b6624a04ff9710
VictorDhein <victordhein@gmail.com>
2024-09-08 11:35:33 -0300

    Lógica de programação

---
+++ Tarefa modulo 6/TarefaModuloSeis.java
@@ -0,0 +1,26 @@
+Logica de programação ao acordar
+
+	1 - abre os olhos
+	2 - enrola na cama para se levantar
+	3 - levanta da cama
+	4 - coloca o chinelo
+	5 - vai até a cozinha
+	6 - inicia o processo de fazer o cafe
+	7 - coloca agua em uma panela 
+	8 - liga o fogão 
+	9 - coloca a panela pra ferver a agua
+	10 - coa o café
+	11 - pega o pão
+	12 - corta o pão
+	13 - passa doce no pão
+	14 - escova os dentes antes de tomar cafe? (SIM = P-14) (NÃO = P-15)	
+			1 - toma cafe
+			2 - come pão
+			3 - escova os dentes
+	15 - toma cafe
+	16 - come pão
+	17 - escova os dentes
+	18 - esta pronto para o dia
+	
+	break
+	
